40dba4ba234a242eacc69373244d3f049948dd55
divyesh <divyeshjivani000@gmail.com>
2025-04-26 00:27:57 +0530

    Added Views,templates and urls and worked around it

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question
+from django.template import loader
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk = question_id)
+    return render(request,"polls/detail.html",{"question" : question})
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
 
 def index(request):
-    return HttpResponse("Hello World, this is polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context,request))
