09e887be67169f10018b03f9e7b04a55948adbd7
divyesh <rajjivani027@gmail.com>
2025-05-12 20:59:01 +0530

    Tried ManyToMany relationship in Django Models

--- storeFront/playground/models.py
+++ storeFront/playground/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
-class Student(models.Model):
+class Person(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    email = models.EmailField()
-    address = models.TextField()
-    image = models.ImageField()
-    file = models.FileField()
 
-class Product(models.Model):
-    pass
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(Person,through="Membership")
+
+    def __str__(self):
+        return self.name
+    
+class Membership(models.Model):
+    person = models.ForeignKey(Person,on_delete=models.CASCADE)
+    group = models.ForeignKey(Group,on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reson = models.CharField(max_length=64)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["person","group"],name="unique_person_group"
+            )
+        ]
 
