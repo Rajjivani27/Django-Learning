b536fe2265d35aa61aee0e8c56d4631acd60ea30
divyesh <rajjivani027@gmail.com>
2025-05-20 12:47:00 +0530

    Tried Multi-table inheritance in Models

--- storeFront/playground/models.py
+++ storeFront/playground/models.py
@@ -1,28 +1,10 @@
 from django.db import models
 
-class Person(models.Model):
-    name = models.CharField(max_length=100)
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.name
-
-class Group(models.Model):
-    name = models.CharField(max_length=100)
-    members = models.ManyToManyField(Person,through="Membership")
-
-    def __str__(self):
-        return self.name
-    
-class Membership(models.Model):
-    person = models.ForeignKey(Person,on_delete=models.CASCADE)
-    group = models.ForeignKey(Group,on_delete=models.CASCADE)
-    date_joined = models.DateField()
-    invite_reson = models.CharField(max_length=64)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["person","group"],name="unique_person_group"
-            )
-        ]
-
+class Restaurant(Place):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+    serves_burger = models.BooleanField(default=False)
