b022f4725aca7ae27b598d90006ebaef4585d646
divyesh <rajjivani027@gmail.com>
2025-05-21 11:55:42 +0530

    Changed many things and tried many things

--- storeFront/storeFront/urls.py
+++ storeFront/storeFront/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from playground import urls
+from playground import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/',include('playground.urls')),
+    path('',include('playground.urls')),
 ]
