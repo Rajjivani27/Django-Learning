8f75b876ec53813557b6aecf6a9058116232f5c4
divyesh <rajjivani027@gmail.com>
2025-05-26 23:00:38 +0530

    Updated

--- storeFront/storeFront/urls.py
+++ storeFront/storeFront/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from playground import urls
 from playground import views
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('playground.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
