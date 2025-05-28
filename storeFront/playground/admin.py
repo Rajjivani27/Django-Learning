f518c45e6b495ce4d14314f176f09021d5fb5ba6
divyesh <rajjivani027@gmail.com>
2025-05-28 22:55:03 +0530

    Updated

--- storeFront/playground/admin.py
+++ storeFront/playground/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Department)
+admin.site.register(StudentID)
+admin.site.register(Student)
 
 # Register your models here.
