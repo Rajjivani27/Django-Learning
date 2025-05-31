fdf5f9fb3946f2c891c4d98901d40afcb0938507
divyesh <rajjivani027@gmail.com>
2025-05-31 22:50:24 +0530

    Started Project Report Card

---
+++ mysite/reportcard/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+class Department(models.Model):
+    department = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.department
+    
+    class Meta:
+        ordering = ['department']
+
+class StudentID(models.Model):
+    student_id = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.student_id
+    
+class Student(models.Model):
+    department = models.ForeignKey(Department,related_name="depart",on_delete=models.CASCADE)
+    student_id = models.OneToOneField(StudentID,related_name="studentid",on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=100)
+    student_email = models.CharField(max_length=100)
+    student_age = models.IntegerField(default=18)
+    student_address = models.TextField()
+
+    def __str__(self) -> str:
+        return self.student_name
+    
+    class Meta:
+        ordering = ['student_name']
+        verbose_name = "student"
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=50)
+
+    def __str__(self)->str:
+        return self.subject_name
+
+class SubjectMarks(models.Model):
+    student = models.ForeignKey(Student,related_name="studentmarks",on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject,related_name="subjectmarks",on_delete=models.CASCADE)
+    marks = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.student.student_name}  {self.subject.subject_name}'
+
+    class Meta:
+        unique_together = ['student','subject']
---
+++ mysite/reportcard/seed.py
@@ -0,0 +1,45 @@
+from faker import Faker
+import random
+from .models import *
+
+fake = Faker()
+
+def create_subject_marks() -> None:
+    try:
+        students = Student.objects.all()
+        for student in students:
+            subjects = Subject.objects.all()
+            for subject in subjects:
+                SubjectMarks.objects.create(
+                    student = student,
+                    subject = subject,
+                    marks = random.randint(0,100)
+                )
+    except Exception as e:
+        print(e)
+
+def seed_db(n=100) -> None:
+    try:
+        departments_obj = Department.objects.all()
+        for _ in range(n):
+            random_index = random.randint(0 , len(departments_obj)-1)
+
+            department = departments_obj[random_index]
+            student_id = f'STU-0{random.randint(100,999)}'
+            student_name = fake.name()
+            student_email = fake.email()
+            student_age = random.randint(20,30)
+            student_address = fake.address()
+
+            studentid_obj = StudentID.objects.create(student_id = student_id)
+
+            student = Student.objects.create(
+                department = department,
+                student_id = studentid_obj,
+                student_name = student_name,
+                student_email = student_email,
+                student_age = student_age,
+                student_address = student_address
+            )
+    except Exception as e:
+        print(e)
