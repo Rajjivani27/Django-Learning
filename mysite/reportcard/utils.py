428c0bfef20a45c37007dc00232d895f14a2c997
divyesh <rajjivani027@gmail.com>
2025-06-08 20:28:19 +0530

    Tried to sending email with attachments

--- mysite/reportcard/utils.py
+++ mysite/reportcard/utils.py
@@ -1,18 +1,23 @@
-from .models import Student
-import time
-from django.core.mail import send_mail
+from django.core.mail import send_mail,EmailMessage
 from mysite.settings import *
 
 def send_email():
-    subject =  "Subject of the mail"
-    message = "Message you want to send to the other party"
-    from_email = EMAIL_HOST_USER #Email Address from you are sending email(You have to add this in settings.py file)
-    recepient_list = ["mail_of_receiver"]
+    subject = "This is testing mail"
+    message = "Testing django send_mail() functionality"
+    from_email = EMAIL_HOST_USER
+    recipient_list = ['email_of_receiver']
+    send_mail(subject,message,from_email,recipient_list)
+    send_mail_with_attachment(subject,message,from_email,recipient_list)
 
-    send_mail(
-        subject,
-        message,
-        from_email,
-        recepient_list,
-        fail_silently=False
-    )
+def send_mail_with_attachment(subject , message ,from_email,recipient_list):
+    mail = EmailMessage(
+            subject = subject,
+            body = message,
+            from_email= from_email,
+            to=recipient_list,
+            bcc=['email_of_person_for_bcc(optional)'],
+        )
+    
+    mail.attach_file(f"{BASE_DIR}/100d_leetcode.png")
+
+    mail.send()
